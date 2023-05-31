5826ab08bd132ddfacb7a56759e77c59f3c89aa6
pravin mane <122874987+pravinmane0@users.noreply.github.com>
2023-05-31 18:24:12 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+// import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 //const avatarSrc = "https://avatars.githubusercontent.com/u/25058652";
